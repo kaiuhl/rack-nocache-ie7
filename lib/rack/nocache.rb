d5431631fa9a4c6a6cfbf32d27ad089c8624e59f
Kyle Meyer <kaiuhl@gmail.com>
2012-07-12 11:06:17 -0700

    Limit nocache to IE7

--- lib/rack/nocache.rb
+++ lib/rack/nocache.rb
@@ -6,22 +6,33 @@ module Rack
 
     def call(env)
       status, headers, body = @app.call(patch_request_headers(env))
-      [status, patch_response_headers(headers), body]
+      [status, patch_response_headers(headers, env), body]
     end
 
   protected
     CACHE_BUSTER = {
-      "Cache-Control" => "no-cache, no-store, max-age=0, must-revalidate",
+      "Cache-Control" => "no-cache, no-store, max-age=0, must-revalidate, post-check=0, pre-check=0",
       "Pragma" => "no-cache",
       "Expires" => "Fri, 29 Aug 1997 02:14:00 EST"
     }
 
-    def patch_request_headers(env)
-      env.reject{|k,v| k =~ /^HTTP_IF/i }
+    def ie?(env)
+      env["HTTP_USER_AGENT"].to_s.include? "MSIE 7"
     end
 
-    def patch_response_headers(env)
-      env.reject{|k,v| k =~ /^ETag$/i }.merge(CACHE_BUSTER)
+    def patch_request_headers(env)
+      env.reject! { |k,v| k =~ /^HTTP_IF/i } if ie?(env)
+      env
+    end
+
+    def patch_response_headers(headers, env)
+      if ie?(env)
+        headers.reject! { |k,v| k =~ /^ETag$/i }
+        headers ||= {}
+        headers.merge!(CACHE_BUSTER)
+      end
+      headers
     end
   end
 end
+
